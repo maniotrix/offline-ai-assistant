37579a29a5880900b5a890a5f53df4f744578133
Prince <prince.mst13@itbhu.ac.in>
2025-03-31 17:19:37 +0530

    update

--- karna-python-backend/inference/omniparser/task_schema.py
+++ karna-python-backend/inference/omniparser/task_schema.py
@@ -19,3 +19,26 @@ class Task(BaseModel):
     app_type: str
     app_url: str
     steps: List[Step]
+    
+def load_task_schema_from_json(json_file_path: str):
+    with open(json_file_path, 'r') as file:
+        return Task.model_validate_json(file.read())
+
+class TaskPlanner():
+    task_schema: Task
+    def __init__(self, task_schema: Task):
+        self.task_schema = task_schema
+
+    
+            
+            
+if __name__ == "__main__":
+    # load the task schema from the json file
+    import os
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    task_schema = load_task_schema_from_json(os.path.join(current_dir, "chat_with_chatgpt.json"))
+    task_planner = TaskPlanner(task_schema)
+    # pretty print the task schema
+    print(task_planner.task_schema.model_dump_json(indent=4))
+
+
